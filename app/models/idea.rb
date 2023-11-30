2664b343ce64e2f78155e37d21eb0e7035d2be46
elisheva <elisheva.benisaac@gmail.com>
2023-11-30 22:13:52 +0200

    Add picture uploads

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
 class Idea < ApplicationRecord
+#This mount_uploader line tells the Idea model that the picture field is a file upload. 
+#It will store information about the file upload on that field to display it later.
+mount_uploader :picture, PictureUploader
 end
