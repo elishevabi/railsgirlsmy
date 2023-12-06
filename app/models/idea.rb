840a8c7d466f03114dba0df7be8d22ceaa8948b4
elisheva <elisheva.benisaac@gmail.com>
2023-12-06 21:32:27 +0200

    More styling to the pages my own

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ApplicationRecord
+validates :name, presence: true
 #This mount_uploader line tells the Idea model that the picture field is a file upload. 
 #It will store information about the file upload on that field to display it later.
 mount_uploader :picture, PictureUploader
